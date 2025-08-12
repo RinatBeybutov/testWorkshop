d1e1ee1ae55dd88aff5737c95c6ec2064e56a3d6
Ринат <ignat934@gmail.com>
2025-08-12 13:09:28 +0300

    1: - Добавлена возможность создания задачи

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,9 +2,9 @@ package org.example;
 
 public class Main {
 
-
   public static void main(String[] args) {
-    var a = 5;
+    TodoManager manager = new TodoManagerImpl();
 
+    manager.create("Первая задача", false);
   }
 }
